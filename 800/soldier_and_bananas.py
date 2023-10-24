30c02d465004f1a21f002997fa662d3f5a69876e
coder7475 <robiulhossain7475@gmail.com>
2023-10-24 20:42:57 +0600

    feat: solve the problem: 59A word

--- 800/soldier_and_bananas.py
+++ 800/soldier_and_bananas.py
@@ -1,2 +1,13 @@
 k, n, w = map(int, input().split())
 
+sum = 0
+
+for i in range(1, w+1):
+  sum += (i * k)
+
+if sum <= n:
+  print(0)
+else:
+  print(sum - n)
+
+# print(k, n, w)
---
+++ 800/word_59A.py
@@ -0,0 +1,14 @@
+word = input()
+upper = 0
+lower = 0
+
+for char in word:
+  if char.isupper():
+    upper += 1
+  else:
+    lower += 1
+
+if lower >= upper:
+  print(word.lower())
+else:
+  print(word.upper())
